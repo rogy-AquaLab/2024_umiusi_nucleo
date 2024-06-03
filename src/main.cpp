2ac783037e32983d64fab5437d0894e7765495fe
H1rono <hronok66@gmail.com>
2024-06-03 17:07:57 +0900

    Apply format

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@
 struct DeferedDelay {
     const uint16_t duration_ms;
 
-    DeferedDelay(uint16_t duration_ms): duration_ms(duration_ms) {}
+    DeferedDelay(uint16_t duration_ms) : duration_ms(duration_ms) {}
 
     ~DeferedDelay() {
         ThisThread::sleep_for(std::chrono::milliseconds(duration_ms));
@@ -72,10 +72,10 @@ int main() {
         PwmOut(BLDC4_PIN),
     };
     PwmOut servos[4] = {
-            PwmOut(SERVO1_PIN),
-            PwmOut(SERVO2_PIN),
-            PwmOut(SERVO3_PIN),
-            PwmOut(SERVO4_PIN),
+        PwmOut(SERVO1_PIN),
+        PwmOut(SERVO2_PIN),
+        PwmOut(SERVO3_PIN),
+        PwmOut(SERVO4_PIN),
     };
     BufferedSerial pc(USBTX, USBRX);
     // Init modules
@@ -86,13 +86,13 @@ int main() {
     // fake; TODO
     uint16_t flexsensor1_value = 0xF001;
     uint16_t flexsensor2_value = 0xF020;
-    uint16_t current_value = 0x0CAE;
-    uint16_t voltage_value = 0xAE00;
+    uint16_t current_value     = 0x0CAE;
+    uint16_t voltage_value     = 0xAE00;
     while (true) {
         DeferedDelay _delay(10);
         pc.sync();
         uint8_t header = 0;
-        ssize_t read = pc.read(&header, 1);
+        ssize_t read   = pc.read(&header, 1);
         if (read < 1) {
             continue;
         }
@@ -104,30 +104,32 @@ int main() {
             for (size_t i = 0; i < 4; ++i) {
                 uint16_t pulsewidth_us_lsb = static_cast<uint16_t>(buffer[i * 2 + 0]);
                 uint16_t pulsewidth_us_msb = static_cast<uint16_t>(buffer[i * 2 + 1]);
-                bldcs[i].pulsewidth_us((pulsewidth_us_lsb << 0) | (pulsewidth_us_msb << 8));
+                bldcs[i].pulsewidth_us(
+                    (pulsewidth_us_lsb << 0) | (pulsewidth_us_msb << 8)
+                );
             }
             for (size_t i = 0; i < 4; ++i) {
                 uint16_t pulsewidth_us_lsb = static_cast<uint16_t>(buffer[i * 2 + 0 + 8]);
                 uint16_t pulsewidth_us_msb = static_cast<uint16_t>(buffer[i * 2 + 1 + 8]);
-                servos[i].pulsewidth_us((pulsewidth_us_lsb << 0) | (pulsewidth_us_msb << 8));
+                servos[i].pulsewidth_us(
+                    (pulsewidth_us_lsb << 0) | (pulsewidth_us_msb << 8)
+                );
             }
-        }
-            break;
+        } break;
         case 1: {
             // read
             uint8_t buffer[8] = {
-                    static_cast<uint8_t>((flexsensor1_value >> 0) & 0xFF),
-                    static_cast<uint8_t>((flexsensor1_value >> 8) & 0xFF),
-                    static_cast<uint8_t>((flexsensor2_value >> 0) & 0xFF),
-                    static_cast<uint8_t>((flexsensor2_value >> 8) & 0xFF),
-                    static_cast<uint8_t>((current_value >> 0) & 0xFF),
-                    static_cast<uint8_t>((current_value >> 8) & 0xFF),
-                    static_cast<uint8_t>((voltage_value >> 0) & 0xFF),
-                    static_cast<uint8_t>((voltage_value >> 8) & 0xFF),
+                static_cast<uint8_t>((flexsensor1_value >> 0) & 0xFF),
+                static_cast<uint8_t>((flexsensor1_value >> 8) & 0xFF),
+                static_cast<uint8_t>((flexsensor2_value >> 0) & 0xFF),
+                static_cast<uint8_t>((flexsensor2_value >> 8) & 0xFF),
+                static_cast<uint8_t>((current_value >> 0) & 0xFF),
+                static_cast<uint8_t>((current_value >> 8) & 0xFF),
+                static_cast<uint8_t>((voltage_value >> 0) & 0xFF),
+                static_cast<uint8_t>((voltage_value >> 8) & 0xFF),
             };
             pc.write(buffer, 8);
-        }
-        break;
+        } break;
         case 0xFF:
             // quit
             goto END;
@@ -136,6 +138,6 @@ int main() {
             continue;
         }
     }
-    END:
+END:
     return 0;
 }
