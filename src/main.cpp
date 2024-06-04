68c9165ff70462541376aafa7379947d55d2b87d
H1rono <hronok66@gmail.com>
2024-06-04 10:26:50 +0900

    Add sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 
 #include "mbed.h"
+#include "ThisThread.h"
 
 // Pin Map:
 // targets/TARGET_STM/TARGET_STM32F3/TARGET_STM32F3x8/TARGET_NUCLEO_F303K8/PeripheralPins.c
@@ -96,6 +97,8 @@ int main() {
         if (read < 1) {
             continue;
         }
+        // なぜかこれがないと動かない
+        ThisThread::sleep_for(20ms);
         switch (header) {
         case 0: {
             // write
