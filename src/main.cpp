8ec8f9a20291257363abfd21a7567365a75d6bf4
H1rono <hronok66@gmail.com>
2024-08-11 09:52:05 +0900

    :fire: Remove Machine

--- src/main.cpp
+++ src/main.cpp
@@ -65,132 +65,96 @@ public:
     }
 };
 
-class Machine {
-private:
-    static constexpr size_t INPUTS_THREAD_STACK_SIZE = 1024;
-    static constexpr size_t SETUP_THREAD_STACK_SIZE  = 512;
+int main() {
+    constexpr size_t INPUTS_THREAD_STACK_SIZE = 1024;
+    constexpr size_t SETUP_THREAD_STACK_SIZE  = 512;
 
-    CachedInputs         inputs;
-    OutputMachine        output;
-    mbed::BufferedSerial pc;
+    CachedInputs         inputs{};
+    OutputMachine        output{};
+    mbed::BufferedSerial pc(USBTX, USBRX);
 
-    std::array<unsigned char, SETUP_THREAD_STACK_SIZE>  setup_thread_stack;
-    std::array<unsigned char, INPUTS_THREAD_STACK_SIZE> inputs_thread_stack;
+    std::array<unsigned char, SETUP_THREAD_STACK_SIZE>  setup_thread_stack{};
+    std::array<unsigned char, INPUTS_THREAD_STACK_SIZE> inputs_thread_stack{};
 
-    rtos::Thread setup_thread;
-    rtos::Thread inputs_thread;
+    rtos::Thread setup_thread(
+        osPriorityNormal, SETUP_THREAD_STACK_SIZE, setup_thread_stack.data()
+    );
+    rtos::Thread inputs_thread(
+        osPriorityNormal, INPUTS_THREAD_STACK_SIZE, inputs_thread_stack.data()
+    );
 
-    void op_write_outputs() {
-        std::array<uint8_t, 16> buffer{}; // FIXME: 16 == THRUSTER_NUM * 2 * 2
-        pc.read(buffer.data(), 16);
-        std::array<std::pair<uint16_t, uint16_t>, THRUSTER_NUM> pulsewidths_us{};
-        for (size_t i = 0; i < THRUSTER_NUM; ++i) {
-            // bldc
-            uint16_t pulsewidth_us_lsb = static_cast<uint16_t>(buffer[i * 2 + 0]);
-            uint16_t pulsewidth_us_msb = static_cast<uint16_t>(buffer[i * 2 + 1]);
-            pulsewidths_us[i].first = (pulsewidth_us_lsb << 0) | (pulsewidth_us_msb << 8);
-            // servo
-            pulsewidth_us_lsb = static_cast<uint16_t>(buffer[i * 2 + 0 + THRUSTER_NUM * 2]
-            );
-            pulsewidth_us_msb = static_cast<uint16_t>(buffer[i * 2 + 1 + THRUSTER_NUM * 2]
-            );
-            pulsewidths_us[i].second = (pulsewidth_us_lsb << 0)
-                                       | (pulsewidth_us_msb << 8);
-        }
-        this->output.set_powers(pulsewidths_us);
-    }
-
-    void op_read_inputs() {
-        std::array<uint8_t, 8> buffer = inputs.get().packet_data();
-        pc.write(buffer.data(), 8);
-    }
-
-    void op_read_state() {
-        uint8_t state_val = static_cast<uint8_t>(this->output.state());
-        this->pc.write(&state_val, 1);
-    }
-
-    auto op_trigger_setup() -> osStatus {
-        if (this->output.state() == State::INITIALIZING) {
-            return osOK;
-        }
-        OutputMachine* om = &this->output;
-        return this->setup_thread.start([om]() {
-            om->initialize();
-        });
-    }
-
-    void op_suspend() {
-        this->output.suspend();
-    }
-
-public:
-    explicit Machine() :
-        inputs(),
-        output(),
-        pc(USBTX, USBRX),
-        setup_thread_stack(),
-        inputs_thread_stack(),
-        setup_thread(
-            osPriorityNormal, SETUP_THREAD_STACK_SIZE, setup_thread_stack.data()
-        ),
-        inputs_thread(
-            osPriorityNormal, INPUTS_THREAD_STACK_SIZE, inputs_thread_stack.data()
-        ) {}
-
-    auto start_inputs() -> osStatus {
-        return this->inputs_thread.start([this] {
-            this->inputs.read();
+    osStatus status = inputs_thread.start([&inputs]() {
+        while (true) {
+            inputs.read();
             rtos::ThisThread::sleep_for(10ms);
-        });
+        }
+    });
+    if (status != osOK) {
+        // あってはならないが
+        inputs.read();
     }
-
-    void loop() {
+    while (true) {
         DeferedDelay _delay(10);
         pc.sync();
         uint8_t header = 0;
         // TODO: timeout
         ssize_t read = pc.read(&header, 1);
         if (read < 1) {
-            this->op_suspend();
-            return;
+            output.suspend();
+            continue;
         }
         // なぜかこれがないと動かない
         rtos::ThisThread::sleep_for(20ms);
         switch (header) {
-        case 0:
+        case 0: {
             // write
-            this->op_write_outputs();
-            break;
-        case 1:
+            std::array<uint8_t, 16> buffer{}; // FIXME: 16 == THRUSTER_NUM * 2 * 2
+            pc.read(buffer.data(), 16);
+            std::array<std::pair<uint16_t, uint16_t>, THRUSTER_NUM> pulsewidths_us{};
+            for (size_t i = 0; i < THRUSTER_NUM; ++i) {
+                // bldc
+                uint16_t pulsewidth_us_lsb = static_cast<uint16_t>(buffer[i * 2 + 0]);
+                uint16_t pulsewidth_us_msb = static_cast<uint16_t>(buffer[i * 2 + 1]);
+                pulsewidths_us[i].first    = (pulsewidth_us_lsb << 0)
+                                          | (pulsewidth_us_msb << 8);
+                // servo
+                pulsewidth_us_lsb
+                    = static_cast<uint16_t>(buffer[i * 2 + 0 + THRUSTER_NUM * 2]);
+                pulsewidth_us_msb
+                    = static_cast<uint16_t>(buffer[i * 2 + 1 + THRUSTER_NUM * 2]);
+                pulsewidths_us[i].second = (pulsewidth_us_lsb << 0)
+                                           | (pulsewidth_us_msb << 8);
+            }
+            output.set_powers(pulsewidths_us);
+        } break;
+        case 1: {
             // read
-            this->op_read_inputs();
-            break;
-        case 2:
+            std::array<uint8_t, 8> buffer = inputs.get().packet_data();
+            pc.write(buffer.data(), 8);
+        } break;
+        case 2: {
             // read state
-            this->op_read_state();
-            break;
-        case 0xFE:
-            // (re)start
-            // TODO: handle returned osStatus
-            this->op_trigger_setup();
-            break;
+            uint8_t state_val = static_cast<uint8_t>(output.state());
+            pc.write(&state_val, 1);
+        } break;
+        case 0xFE: {
+            // read state
+            if (output.state() == State::INITIALIZING) {
+                return osOK;
+            }
+            // TODO: handle osStatus
+            osStatus _ = setup_thread.start([&output]() {
+                output.initialize();
+            });
+        } break;
         case 0xFF:
             // suspend
-            this->op_suspend();
+            output.suspend();
             break;
         default:
             // unexpected
-            return;
+            continue;
         }
-    }
-};
-
-int main() {
-    Machine machine{};
-    machine.start_inputs();
-    while (true) {
-        machine.loop();
     }
     return 0;
 }
